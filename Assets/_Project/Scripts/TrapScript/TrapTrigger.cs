5bbcf3172c3d41428dc1cfe2f3a16567c3d73cd0
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-01-17 16:30:49 +0530

    Added animation for fox trap with adjustment in the TrapTrigger code

--- Assets/_Project/Scripts/TrapScript/TrapTrigger.cs
+++ Assets/_Project/Scripts/TrapScript/TrapTrigger.cs
@@ -7,11 +7,27 @@ public class TrapTrigger : MonoBehaviour
 {
     public UnityEvent OnTrapTriggered;
 
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            _animator.SetBool("isTrapTriggered", true);
             OnTrapTriggered.Invoke();
+            StartCoroutine(WaitForAnimationToEnd());
         }
+    }
+
+    private IEnumerator WaitForAnimationToEnd()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        Destroy(gameObject);
     }
 }
